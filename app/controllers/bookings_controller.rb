4f9f855e35290cc865d8cffd4f2dda23391a6f03
Jobrann Mous <jobrann.mous@hotmail.com>
2017-11-28 15:14:32 +0100

    finished the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,26 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.all
-  end
-
-  def show
-    @flat = Flat.find(params[:flat_id])
-    @bookings = Booking.find(params[:booking_id])
-  end
-
-  def new
-
+    @bookings = current_user.bookings
   end
 
   def create
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.flat = @flat
+    @booking.nights = @booking.end_date - @booking.start_date
+    if @booking.save
+      redirect_to bookings_path
+    else
+      redirect_to flat_path(@flat)
+    end
+  end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.delete
+    redirect_to bookings_path
   end
 
   private
