8d7ccb132931c75c51d67d08ea2945441c849477
Clement Flinois <clement.flinois@gmail.com>
2017-11-28 16:01:11 +0100

    page contact + suppression des notice et alerte sur la page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
   resources :flats do
     resources :bookings, only: [ :index, :new, :create, :show ]
   end
