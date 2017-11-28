fc635f99a83c442f8bf33039f36d0635cc15488e
adrien moison <adrimoison@gmail.com>
2017-11-28 14:58:37 +0100

    coloumn-name

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -43,7 +43,7 @@ class FlatsController < ApplicationController
 
   private
   def flat_params
-    params.require(:flat).permit(:address, :description, :rooms, :price)
+    params.require(:flat).permit(:address, :description, :rooms, :price, :name)
   end
 
 end
