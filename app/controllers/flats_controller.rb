a005219154b080e16559ad072b428b33ebb93493
Jobrann Mous <jobrann.mous@hotmail.com>
2017-11-29 11:15:00 +0100

    added access to flats page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,7 @@
 class FlatsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @flats = policy_scope(Flat).order(created_at: :desc)
   end
