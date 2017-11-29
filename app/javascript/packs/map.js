0830f4a1e1f357436fb55cffb09f1fd2454ea967
Lola Barbier <lolabarbier@hotmail.com>
2017-11-29 14:50:38 +0100

    added style to show view map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,75 +1,62 @@
 import GMaps from 'gmaps/gmaps.js';
 
-const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers)
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
-}
-
-const styles = [ {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 60
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 40
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.province",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 30
-            }
-        ]
-    },
+const styles = [
+  {
+    "featureType":
+      "landscape",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 60
+        }
+      ]
+  },
+  {
+    "featureType": "road.local",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 40
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+  },
+  {
+    "featureType": "transit",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
+  },
+  {
+      "featureType": "administrative.province",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "stylers": [
+          {
+              "visibility": "on"
+          },
+          {
+              "lightness": 30
+          }
+      ]
+  },
     {
         "featureType": "road.highway",
         "elementType": "geometry.fill",
@@ -107,9 +94,24 @@ const styles = [ {
         ]
     },
     {} ];
+const mapElement = document.getElementById('map');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers)
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+  map.addStyle({
+    styles: styles,
+    mapTypeId: 'map_style'
+  });
+  map.setStyle('map_style');
+}
 
-map.addStyle({
-  styles: styles,
-  mapTypeId: 'map_style'
-});
-map.setStyle('map_style');
+
