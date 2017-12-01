6fb15ed97dc77c3f570490d35b0b1cbdba6077a6
Jobrann Mous <jobrann.mous@hotmail.com>
2017-12-01 14:42:10 +0100

    fixed date format

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/themes/airbnb.css'
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import 'flatpickr/dist/themes/airbnb.css';
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr("#range_start", {
   altInput: true,
